ce2345e37566783b8ca1f2f81e827d39886d60d3
Rossy1995 <lordross11@googlemail.com>
2017-08-07 10:23:54 +0100

    Updated code to support Estimote SDK

--- app/src/main/java/com/example/rossmaguire/beacontest/MainActivity.java
+++ app/src/main/java/com/example/rossmaguire/beacontest/MainActivity.java
@@ -1,13 +1,45 @@
 package com.example.rossmaguire.beacontest;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+import com.estimote.sdk.BeaconManager;
+import com.estimote.sdk.Region;
+import com.estimote.sdk.SystemRequirementsChecker;
+
+import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BeaconManager beaconManager;
+    private Region region;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        beaconManager = new BeaconManager(this);
+        region = new Region("GC", UUID.fromString("B9407F30-F5F8-466E-AFF9-25556B57FE6D"), null, null);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        SystemRequirementsChecker.checkWithDefaultDialogs(this);
+        beaconManager.connect(new BeaconManager.ServiceReadyCallback() {
+            @Override
+            public void onServiceReady() {
+                beaconManager.startRanging(region);
+            }
+        });
+    }
+
+    @Override
+    protected void onPause() {
+        beaconManager.stopRanging(region);
+        super.onPause();
     }
 }
+
