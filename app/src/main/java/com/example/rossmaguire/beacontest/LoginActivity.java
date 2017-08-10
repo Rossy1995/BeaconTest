1a634ed7ee2d182e1b8e114b4a36cd1d8ec5578a
Rossy1995 <lordross11@googlemail.com>
2017-08-10 09:36:01 +0100

    Migrated from MS SQL connection to MySQL connection

--- app/src/main/java/com/example/rossmaguire/beacontest/LoginActivity.java
+++ app/src/main/java/com/example/rossmaguire/beacontest/LoginActivity.java
@@ -1,106 +1,174 @@
 package com.example.rossmaguire.beacontest;
 
-import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.database.SQLException;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.view.inputmethod.InputMethodManager;
-import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ProgressBar;
 import android.widget.Toast;
-
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class LoginActivity extends AppCompatActivity {
 
-    EditText username, password;
-    Button btnLogin;
-    DBConnection dbConnection;
-    ProgressBar progBar;
-
+    public static final int CONNECTION_TIMEOUT=10000;
+    public static final int READ_TIMEOUT=15000;
+    private EditText username, password;
+    private Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        dbConnection = new DBConnection();
         username = (EditText) findViewById(R.id.userName);
         password = (EditText) findViewById(R.id.passWord);
         btnLogin = (Button) findViewById(R.id.btnLogin);
-        progBar = (ProgressBar) findViewById(R.id.progBar);
-        progBar.setVisibility(View.GONE);
-
-        btnLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Login login = new Login();
-                login.execute("");
-            }
-        });
     }
 
-    public class Login extends AsyncTask<String, String, String> {
-        String z = "";
-        Boolean isSuccess = false;
+    public void checkLogin(View arg0) {
 
-        String user = username.getText().toString();
-        String pass = password.getText().toString();
+        // Get text from email and passord field
+        final String user = username.getText().toString();
+        final String pass = password.getText().toString();
+
+        // Initialize  AsyncLogin() class with email and password
+        new AsyncLogin().execute(user,pass);
+    }
+
+    private class AsyncLogin extends AsyncTask<String, String, String> {
+        ProgressDialog pdLoading = new ProgressDialog(LoginActivity.this);
+        HttpURLConnection conn;
+        URL url = null;
 
         @Override
         protected void onPreExecute() {
-            progBar.setVisibility(View.VISIBLE);
-        }
+            super.onPreExecute();
 
-        @Override
-        protected void onPostExecute(String r) {
-            progBar.setVisibility(View.GONE);
-            Toast.makeText(LoginActivity.this, r, Toast.LENGTH_SHORT).show();
+            //this method will be running on UI thread
+            pdLoading.setMessage("\tLoading...");
+            pdLoading.setCancelable(false);
+            pdLoading.show();
 
-            if (isSuccess) {
-                Intent i = new Intent(LoginActivity.this, MainActivity.class);
-                startActivity(i);
-                finish();
-            }
         }
 
         @Override
         protected String doInBackground(String... params) {
-            if (user.trim().equals("") || pass.trim().equals("")) {
-                z = "Please enter your username and password";
-            } else {
-                try {
-                    Connection con = dbConnection.CONN();
-                    if (con == null) {
-                        z = "Error in connection with SQL server";
-                    } else {
-                        String query = "Select * from Users where convert(VARCHAR, Username) = '" + user + "' and convert(VARCHAR, Password) = '" + pass + "' ;";
-                        Statement statement = con.createStatement();
-                        ResultSet rs = statement.executeQuery(query);
+            try {
+                // Enter URL address where your php file resides
+                url = new URL("http://localhost/test/login.inc.php");
 
-                        if (rs.next()) {
-                            z = "Login successful!";
-                            isSuccess = true;
-                        } else {
-                            z = "Invalid credentials";
-                            isSuccess = false;
-                        }
-                    }
-                } catch (Exception ex) {
-                    isSuccess = false;
-                    z = "Exceptions";
-                    Log.e("MYAPP", "exception", ex);
-                }
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+                return "exception";
             }
-            return z;
+            try {
+                // Setup HttpURLConnection class to send and receive data from php and mysql
+                conn = (HttpURLConnection) url.openConnection();
+                conn.setReadTimeout(READ_TIMEOUT);
+                conn.setConnectTimeout(CONNECTION_TIMEOUT);
+                conn.setRequestMethod("POST");
+
+                // setDoInput and setDoOutput method depict handling of both send and receive
+                conn.setDoInput(true);
+                conn.setDoOutput(true);
+
+                // Append parameters to URL
+                Uri.Builder builder = new Uri.Builder()
+                        .appendQueryParameter("username", params[0])
+                        .appendQueryParameter("password", params[1]);
+                String query = builder.build().getEncodedQuery();
+
+                // Open connection for sending data
+                OutputStream os = conn.getOutputStream();
+                BufferedWriter writer = new BufferedWriter(
+                        new OutputStreamWriter(os, "UTF-8"));
+                writer.write(query);
+                writer.flush();
+                writer.close();
+                os.close();
+                conn.connect();
+
+            } catch (IOException e1) {
+                e1.printStackTrace();
+                return "exception";
+            }
+
+            try {
+
+                int response_code = conn.getResponseCode();
+
+                // Check if successful connection made
+                if (response_code == HttpURLConnection.HTTP_OK) {
+
+                    // Read data sent from server
+                    InputStream input = conn.getInputStream();
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+                    StringBuilder result = new StringBuilder();
+                    String line;
+
+                    while ((line = reader.readLine()) != null) {
+                        result.append(line);
+                    }
+
+                    // Pass data to onPostExecute method
+                    return (result.toString());
+
+                } else {
+
+                    return ("unsuccessful");
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+                return "exception";
+            } finally {
+                conn.disconnect();
+            }
+
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+
+            //this method will be running on UI thread
+
+            pdLoading.dismiss();
+
+            if (result.equalsIgnoreCase("true")) {
+                /* Here launching another activity when login successful. If you persist login state
+                use sharedPreferences of Android. and logout button to clear sharedPreferences.
+                 */
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+                LoginActivity.this.finish();
+
+            } else if (result.equalsIgnoreCase("false")) {
+
+                // If username and password does not match display a error message
+                Toast.makeText(LoginActivity.this, "Invalid email or password", Toast.LENGTH_LONG).show();
+
+            } else if (result.equalsIgnoreCase("exception") || result.equalsIgnoreCase("unsuccessful")) {
+
+                Toast.makeText(LoginActivity.this, "Connection Problem with database", Toast.LENGTH_LONG).show();
+
+            }
         }
     }
 }
--- app/src/main/java/com/example/rossmaguire/beacontest/MainActivity.java
+++ app/src/main/java/com/example/rossmaguire/beacontest/MainActivity.java
@@ -1,61 +1,19 @@
 package com.example.rossmaguire.beacontest;
 
-import android.app.Notification;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.View;
 import android.widget.Button;
-
-
-import com.estimote.sdk.Beacon;
 import com.estimote.sdk.BeaconManager;
 import com.estimote.sdk.Region;
-import com.estimote.sdk.SystemRequirementsChecker;
-
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.List;
-import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
-
-    private static final String TAG = "BeaconNotifications";
-    private BeaconManager beaconManager;
-    private Region region;
-    private static final SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
-    private java.sql.Time time = new java.sql.Time(Calendar.getInstance().getTime().getTime());
-    private DBConnection dbConnection = new DBConnection();
-    private Connection con = dbConnection.CONN();
-    private Button btnInsert;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        btnInsert = (Button) (findViewById(R.id.btnInsert));
-
-        btnInsert.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                try {
-                    String query = "INSERT INTO Timesheets (Check_In_Time) VALUES ('" + time + "');";
-                    Statement statement = con.createStatement();
-                    ResultSet rs = statement.executeQuery(query);
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
     }
 }
 
