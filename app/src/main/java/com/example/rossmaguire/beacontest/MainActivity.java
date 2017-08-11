589e7dc20f009d59ce3c6ca084922f2ca643e075
Rossy1995 <lordross11@googlemail.com>
2017-08-11 16:51:42 +0100

    Updated code to send data to MySQL

--- app/src/main/java/com/example/rossmaguire/beacontest/MainActivity.java
+++ app/src/main/java/com/example/rossmaguire/beacontest/MainActivity.java
@@ -5,21 +5,59 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.estimote.sdk.Beacon;
 import com.estimote.sdk.BeaconManager;
 import com.estimote.sdk.Region;
 
+import net.sourceforge.jtds.jdbc.DateTime;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.sql.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 import java.util.UUID;
+import java.util.jar.Attributes;
 
 public class MainActivity extends AppCompatActivity {
 
     private BeaconManager beaconManager;
-    private TextView userName = (TextView) findViewById(R.id.userName);
+    private String checkIn;
+    private Button btnCheckIn;
+    private SimpleDateFormat sdf = new SimpleDateFormat("YYYY-mm-dd HH:mm:ss");
+    private InputStream is = null;
+    private String result = null;
+    private String line = null;
+    private int code;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,9 +67,11 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = getIntent();
         String username = intent.getStringExtra(LoginActivity.USER_NAME);
 
+        TextView userName = (TextView) findViewById(R.id.userName);
+
         userName.setText("Welcome " + username);
 
-        final Region dev = new Region("dev", UUID.fromString("B9407F30-F5F8-466E-AFF9-25556B57FE6D"), 3640, 4061);
+        /*final Region dev = new Region("dev", UUID.fromString("B9407F30-F5F8-466E-AFF9-25556B57FE6D"), 3640, 4061);
         final Region entrance = new Region("entrance", UUID.fromString("B9407F30-F5F8-466E-AFF9-25556B57FE6D"), 55141, 43349);
 
         beaconManager = new BeaconManager(getApplicationContext());
@@ -92,6 +132,64 @@ public class MainActivity extends AppCompatActivity {
         NotificationManager notificationManager =
                 (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
         notificationManager.notify(1, notification);
+    }*/
+    }
+
+    public void checkInTime(View view) {
+        checkIn = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(Calendar.getInstance().getTime());
+        insertToDatabase(checkIn);
+    }
+
+    private void insertToDatabase(final String checkIn)
+    {
+        class SendPostReqAsyncTask extends AsyncTask<String, Void, String>{
+            @Override
+            protected String doInBackground(String... params){
+                String paramCheckIn = params[0];
+
+                List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+                nameValuePairs.add(new BasicNameValuePair("check_in_time", checkIn));
+
+                try {
+                    HttpClient httpClient = new DefaultHttpClient();
+                    HttpPost httpPost = new HttpPost("http://ssmale.ddns.net/GC/add_check_in_time.php");
+                    httpPost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+                    HttpResponse response = httpClient.execute(httpPost);
+                    HttpEntity entity = response.getEntity();
+
+                    is = entity.getContent();
+
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(is, "UTF-8"), 8);
+                    StringBuilder sb = new StringBuilder();
+
+                    String line = null;
+                    while ((line = reader.readLine()) != null)
+                    {
+                        sb.append(line + "\n");
+                    }
+                    result = sb.toString();
+                }
+                catch (ClientProtocolException e) {
+                    e.printStackTrace();
+                }
+                catch (IOException e){
+                    e.printStackTrace();
+                }
+                return result;
+            }
+
+            @Override
+            protected void onPostExecute(String result){
+                String s = result.trim();
+                if(s.equalsIgnoreCase("success")){
+                    Toast.makeText(getApplicationContext(), "Insert was successful!", Toast.LENGTH_LONG).show();
+                }else {
+                    Toast.makeText(getApplicationContext(), "Insert wasn't successful!", Toast.LENGTH_LONG).show();
+                }
+            }
+        }
+        SendPostReqAsyncTask sendPostReqAsyncTask = new SendPostReqAsyncTask();
+        sendPostReqAsyncTask.execute(checkIn);
     }
 }
 
