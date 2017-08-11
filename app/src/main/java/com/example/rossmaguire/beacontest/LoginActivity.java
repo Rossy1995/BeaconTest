130125037708bdb7da5eb4ddea6177da83987d28
Rossy1995 <lordross11@googlemail.com>
2017-08-11 09:12:50 +0100

    Moved BeaconScanner code back to MainActivity

--- app/src/main/java/com/example/rossmaguire/beacontest/LoginActivity.java
+++ app/src/main/java/com/example/rossmaguire/beacontest/LoginActivity.java
@@ -1,36 +1,39 @@
 package com.example.rossmaguire.beacontest;
 
+import android.app.Dialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Button;
 import android.widget.EditText;
 import android.app.ProgressDialog;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.net.Uri;
 import android.os.AsyncTask;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.Toast;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class LoginActivity extends AppCompatActivity {
 
-    public static final int CONNECTION_TIMEOUT=10000;
-    public static final int READ_TIMEOUT=15000;
     private EditText username, password;
-    private Button btnLogin;
+    private String user, pass;
+
+    public static final String USER_NAME = "USERNAME";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,136 +42,89 @@ public class LoginActivity extends AppCompatActivity {
 
         username = (EditText) findViewById(R.id.userName);
         password = (EditText) findViewById(R.id.passWord);
-        btnLogin = (Button) findViewById(R.id.btnLogin);
     }
 
-    public void checkLogin(View arg0) {
+    public void checkLogin(View view) {
 
         // Get text from email and passord field
-        final String user = username.getText().toString();
-        final String pass = password.getText().toString();
+        user = username.getText().toString();
+        pass = password.getText().toString();
 
         // Initialize  AsyncLogin() class with email and password
-        new AsyncLogin().execute(user,pass);
+        login(user, pass);
     }
 
-    private class AsyncLogin extends AsyncTask<String, String, String> {
-        ProgressDialog pdLoading = new ProgressDialog(LoginActivity.this);
-        HttpURLConnection conn;
-        URL url = null;
+    private void login(final String username, String password) {
 
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
+        class LoginAsync extends AsyncTask<String, Void, String>{
 
-            //this method will be running on UI thread
-            pdLoading.setMessage("\tLoading...");
-            pdLoading.setCancelable(false);
-            pdLoading.show();
+            private Dialog loadingDialog;
 
-        }
-
-        @Override
-        protected String doInBackground(String... params) {
-            try {
-                // Enter URL address where your php file resides
-                url = new URL("http://localhost/test/login.inc.php");
-
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
-                return "exception";
-            }
-            try {
-                // Setup HttpURLConnection class to send and receive data from php and mysql
-                conn = (HttpURLConnection) url.openConnection();
-                conn.setReadTimeout(READ_TIMEOUT);
-                conn.setConnectTimeout(CONNECTION_TIMEOUT);
-                conn.setRequestMethod("POST");
-
-                // setDoInput and setDoOutput method depict handling of both send and receive
-                conn.setDoInput(true);
-                conn.setDoOutput(true);
-
-                // Append parameters to URL
-                Uri.Builder builder = new Uri.Builder()
-                        .appendQueryParameter("username", params[0])
-                        .appendQueryParameter("password", params[1]);
-                String query = builder.build().getEncodedQuery();
-
-                // Open connection for sending data
-                OutputStream os = conn.getOutputStream();
-                BufferedWriter writer = new BufferedWriter(
-                        new OutputStreamWriter(os, "UTF-8"));
-                writer.write(query);
-                writer.flush();
-                writer.close();
-                os.close();
-                conn.connect();
-
-            } catch (IOException e1) {
-                e1.printStackTrace();
-                return "exception";
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+                loadingDialog = ProgressDialog.show(LoginActivity.this, "Please wait", "Loading...");
             }
 
-            try {
+            @Override
+            protected String doInBackground(String... params) {
+                String user = params[0];
+                String pass = params[1];
 
-                int response_code = conn.getResponseCode();
+                InputStream is = null;
+                List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+                nameValuePairs.add(new BasicNameValuePair("username", user));
+                nameValuePairs.add(new BasicNameValuePair("password", pass));
+                String result = null;
 
-                // Check if successful connection made
-                if (response_code == HttpURLConnection.HTTP_OK) {
+                try{
+                    HttpClient httpClient = new DefaultHttpClient();
+                    HttpPost httpPost = new HttpPost(
+                            "http://192.168.0.88:6767/login.php");
+                    httpPost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
 
-                    // Read data sent from server
-                    InputStream input = conn.getInputStream();
-                    BufferedReader reader = new BufferedReader(new InputStreamReader(input));
-                    StringBuilder result = new StringBuilder();
-                    String line;
+                    HttpResponse response = httpClient.execute(httpPost);
 
-                    while ((line = reader.readLine()) != null) {
-                        result.append(line);
+                    HttpEntity entity = response.getEntity();
+
+                    is = entity.getContent();
+
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(is, "UTF-8"), 8);
+                    StringBuilder sb = new StringBuilder();
+
+                    String line = null;
+                    while ((line = reader.readLine()) != null)
+                    {
+                        sb.append(line + "\n");
                     }
-
-                    // Pass data to onPostExecute method
-                    return (result.toString());
-
-                } else {
-
-                    return ("unsuccessful");
+                    result = sb.toString();
+                } catch (ClientProtocolException e) {
+                    e.printStackTrace();
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
-
-            } catch (IOException e) {
-                e.printStackTrace();
-                return "exception";
-            } finally {
-                conn.disconnect();
+                return result;
             }
 
-        }
-
-        @Override
-        protected void onPostExecute(String result) {
-
-            //this method will be running on UI thread
-
-            pdLoading.dismiss();
-
-            if (result.equalsIgnoreCase("true")) {
-                /* Here launching another activity when login successful. If you persist login state
-                use sharedPreferences of Android. and logout button to clear sharedPreferences.
-                 */
-                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                startActivity(intent);
-                LoginActivity.this.finish();
-
-            } else if (result.equalsIgnoreCase("false")) {
-
-                // If username and password does not match display a error message
-                Toast.makeText(LoginActivity.this, "Invalid email or password", Toast.LENGTH_LONG).show();
-
-            } else if (result.equalsIgnoreCase("exception") || result.equalsIgnoreCase("unsuccessful")) {
-
-                Toast.makeText(LoginActivity.this, "Connection Problem with database", Toast.LENGTH_LONG).show();
-
+            @Override
+            protected void onPostExecute(String result){
+                String s = result.trim();
+                loadingDialog.dismiss();
+                if(s.equalsIgnoreCase("success")){
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    intent.putExtra(USER_NAME, username);
+                    finish();
+                    startActivity(intent);
+                }else {
+                    Toast.makeText(getApplicationContext(), "Invalid User Name or Password", Toast.LENGTH_LONG).show();
+                }
             }
         }
+
+        LoginAsync la = new LoginAsync();
+        la.execute(username, password);
+
     }
 }
--- app/src/main/java/com/example/rossmaguire/beacontest/MainActivity.java
+++ app/src/main/java/com/example/rossmaguire/beacontest/MainActivity.java
@@ -1,19 +1,90 @@
 package com.example.rossmaguire.beacontest;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.Button;
+
+import com.estimote.sdk.Beacon;
 import com.estimote.sdk.BeaconManager;
 import com.estimote.sdk.Region;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
+
+import java.util.List;
+import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BeaconManager beaconManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Region dev = new Region("dev", UUID.fromString("B9407F30-F5F8-466E-AFF9-25556B57FE6D"), 3640, 4061);
+        final Region entrance = new Region("entrance", UUID.fromString("B9407F30-F5F8-466E-AFF9-25556B57FE6D"), 55141, 43349);
+
+        beaconManager = new BeaconManager(getApplicationContext());
+
+        beaconManager.setBackgroundScanPeriod(25000, 30000);
+
+        beaconManager.setMonitoringListener(new BeaconManager.MonitoringListener() {
+            @Override
+            public void onEnteredRegion(Region region, List<Beacon> list) {
+                if (region.getIdentifier().equals("dev"))
+                {
+                    showNotification(
+                            "Entered dev floor", "Welcome to GC!");
+                }
+                else if (region.getIdentifier().equals("entrance"))
+                {
+                    showNotification(
+                            "Entered entrance", "Welcome to GC!");
+                }
+            }
+            @Override
+            public void onExitedRegion(Region region) {
+                if (region.getIdentifier().equals("dev"))
+                {
+                    showNotification(
+                            "Exited dev floor", "Goodbye!");
+                }
+                else if (region.getIdentifier().equals("entrance"))
+                {
+                    showNotification(
+                            "Exited entrance", "Goodbye!");
+                }
+            }
+        });
+
+        beaconManager.connect(new BeaconManager.ServiceReadyCallback() {
+            @Override
+            public void onServiceReady() {
+                beaconManager.startMonitoring(dev);
+                beaconManager.startMonitoring(entrance);
+            }
+        });
+    }
+
+    public void showNotification(String title, String message) {
+        Intent notifyIntent = new Intent(this, MainActivity.class);
+        notifyIntent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivities(this, 0,
+                new Intent[] { notifyIntent }, PendingIntent.FLAG_UPDATE_CURRENT);
+        Notification notification = new Notification.Builder(this)
+                .setSmallIcon(android.R.drawable.ic_dialog_info)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setAutoCancel(true)
+                .setContentIntent(pendingIntent)
+                .build();
+        notification.defaults |= Notification.DEFAULT_SOUND;
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(1, notification);
     }
 }
 
